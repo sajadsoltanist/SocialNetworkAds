c1412a5f40d833683ee0428b90db2aa6359bf6c6
sajad soltani <sajadsoltani.st@gmail.com>
2022-07-16 16:38:12 +0430

    Celery Implementation

--- API/views.py
+++ API/views.py
@@ -1,21 +1,14 @@
-from .forms import CustomerForm
-from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from django.core import serializers
-from rest_framework.response import Response
+import pickle
+
+import pandas as pd
+from django.shortcuts import render
 from rest_framework import status
-from django.http import JsonResponse
-from rest_framework.parsers import JSONParser
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .forms import CustomerForm
 from .models import Customer
 from .serializer import CustomerSerializers
-
-import pickle
-import json
-import numpy as np
-from sklearn import preprocessing
-import pandas as pd
-from django.shortcuts import render, redirect
-from django.contrib import messages
 
 
 class CustomerView(viewsets.ModelViewSet):
@@ -51,3 +44,4 @@ def FormView(request):
 
     form = CustomerForm()
     return render(request, 'form.html', {'form': form})
+
